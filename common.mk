1b0e9a13c13ed2372a4b40e191a7b1d601d01dae
Jarl-Penguin <jarlpenguin@outlook.com>
2021-03-20 11:36:34 +0300

    sdm632-common: Retrofit dynamic partitions
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I0cfde771f178011f8151879af041952b8e582109

--- common.mk
+++ common.mk
@@ -36,10 +36,28 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # A/B updater
 AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_product=true \
+    POSTINSTALL_PATH_product=bin/check_dynamic_partitions \
+    FILESYSTEM_TYPE_product=ext4 \
+    POSTINSTALL_OPTIONAL_product=false
+
+AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
+
+PRODUCT_PACKAGES += \
+    check_dynamic_partitions \
+    otapreopt_script \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
+# The following modules are included in debuggable builds only.
+PRODUCT_PACKAGES_DEBUG += \
+    bootctl \
+    update_engine_client
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
@@ -129,6 +147,14 @@ PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey \
     android.hardware.drm@1.3.vendor
 
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
+
+# Fastboot
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1.vendor
@@ -206,6 +232,8 @@ PRODUCT_PACKAGES += \
     init.mmi.overlay.rc \
     init.qcom.rc \
     init.target.rc \
+    fstab.qcom \
+    fstab_ramdisk.qcom \
     ueventd.qcom.rc
 
 # IPACM
